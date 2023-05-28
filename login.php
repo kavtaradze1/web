1dc48500eabd9225c5a338165580659d6b0967cd
kavtaradze1 <43911687+kavtaradze1@users.noreply.github.com>
2023-05-29 01:00:56 +0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="/css/login.css">
+    <title>Document</title>
+</head>
+<body id="top">
+    <header class="header">
+        <ul class="navbar-list"><a>შესვლა</a></ul>
+    </header>
+    <div class="leftdecor">ავტორიზაცია</div>
+</body>
+</html>
